079b8d787012caecb9fc610e9c3ff2f1cc5f5f2b
darjeeling4g <darjeeling4g@gmail.com>
2022-08-25 16:32:46 +0900

    Modify ft_lstmap.c

--- inner_circle/libft/ft_lstmap.c
+++ inner_circle/libft/ft_lstmap.c
@@ -6,21 +6,33 @@
 /*   By: siyang <siyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 22:59:36 by siyang            #+#    #+#             */
-/*   Updated: 2022/08/24 23:12:51 by siyang           ###   ########.fr       */
+/*   Updated: 2022/08/25 16:28:34 by siyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void(*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	int	lst_len;
+	t_list	*start;
+	t_list	*new;
 
-	if (lst == NULL)
-		return NULL;
-	lst_len = ft_lstsize(lst);
-	while (i < lst_len)
+	if (lst == NULL || f == NULL || del == NULL)
+		return (NULL);
+	new = ft_lstnew(f(lst->content));
+	start = new;
+	lst = lst->next;
+	while (lst && start)
 	{
-
+		new = ft_lstnew(f(lst->content));
+		ft_lstadd_back(&start, new);
+		if (new == NULL)
+		{
+			ft_lstclear(&start, del);
+			start = NULL;
+			break ;
+		}
+		lst = lst->next;
 	}
+	return (start);
 }
