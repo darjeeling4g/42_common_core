ec50c4057727c4069fdd4cc1d1e430f47be29b3d
darjeeling4g <darjeeling4g@gmail.com>
2022-11-02 19:37:44 +0900

    GNL

--- inner_circle/get_next_line/get_next_line.c
+++ inner_circle/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: siyang <siyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 10:08:43 by siyang            #+#    #+#             */
-/*   Updated: 2022/10/14 21:39:49 by siyang           ###   ########.fr       */
+/*   Updated: 2022/11/02 19:36:06 by siyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 //#include <stdio.h> 
 //#include <fcntl.h> 
 
-static int	get_until_eol(char *buffer, char **backup, char **result);
+static void	get_until_eol(char *buffer, char **backup, char **result);
 
 char	*get_next_line(int fd)
 {
@@ -22,52 +22,60 @@ char	*get_next_line(int fd)
 	ssize_t		nbyte;
 	char		*buffer;
 	char		*result;
-	int			eol;
 
-	result = malloc(sizeof(char) * 1);
-	result[0] = 0;
-	eol = 0;
+	result = ft_strdup("");
+	if (result == NULL)
+	{
+		free(result);
+		return(NULL);
+	}
 	if (backup)
 	{
 		buffer = backup;
-		eol = get_until_eol(buffer, &backup, &result);
+		get_until_eol(buffer, &backup, &result);
 		free(buffer);
 	}
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (buffer == NULL)
+	{
+		free(buffer);
+		free(result);
 		return (NULL);
+	}
 	buffer[BUFFER_SIZE] = '\0';
-	while (eol == 0)
+	while (!backup)
 	{
 		nbyte = read(fd, buffer, BUFFER_SIZE);
-		if (nbyte < 0)
+		if (nbyte < 0 || (nbyte == 0 && *result == '\0'))
+		{
+			free(result);
+			free(buffer);
 			return (NULL);
+		}
 		else if (nbyte == 0)
-			return (result);
+			break ;
 		else
-			eol = get_until_eol(buffer, &backup, &result);
+			get_until_eol(buffer, &backup, &result);
 	}
 	free(buffer);
 	return (result);
 }
 
-static int	get_until_eol(char *buffer, char **backup, char **result)
+static void	get_until_eol(char *buffer, char **backup, char **result)
 {
 	char	*temp;
-	int		eol;
 
-	eol = 0;
 	temp = ft_strchr(buffer, '\n');
 	if (temp != NULL)
 	{
 		*backup = ft_strdup(temp + 1);
 		temp[1] = '\0';
-		eol = 1;
 	}
+	else
+		*backup = NULL;
 	temp = *result;
 	*result = ft_strjoin(*result, buffer);
 	free(temp);
-	return (eol);
 }
 
 //int	main()
@@ -76,6 +84,5 @@ static int	get_until_eol(char *buffer, char **backup, char **result)
 //	fd = open("test", O_RDWR);
 //	printf("%s", get_next_line(fd));
 //	printf("%s", get_next_line(fd));
-//	printf("%s", get_next_line(fd));
-//	printf("%s", get_next_line(fd));
+//	system("leaks a.out");
 //}
