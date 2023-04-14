6493dc8b3cf48f3fcb09ad6ad9ca816b56312d75
darjeeling4g <darjeeling4g@gmail.com>
2023-04-14 17:17:27 +0900

    Add struct

--- inner_circle/philosophers/philo/philo.c
+++ inner_circle/philosophers/philo/philo.c
@@ -6,23 +6,65 @@
 /*   By: siyang <siyang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 19:54:14 by siyang            #+#    #+#             */
-/*   Updated: 2023/04/08 22:27:04 by siyang           ###   ########.fr       */
+/*   Updated: 2023/04/14 17:17:10 by siyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	*philo_loop(void *info)
+{
+	(void)info;
+	printf("i'm philosopher\n");
+	return (NULL);
+}
+
+void	monitoring_loop()
+{
+	while (1);
+}
+
 int	main(int argc, char **argv)
 {
 	struct timeval	time;
 	pthread_t		*philo;
+	int				i;
 
+	(void)argv;
 	if (argc != 5 && argc != 6)
 		return (1);
 	gettimeofday(&time, NULL);
-	philo = malloc(sizeof(pthread_t * number_of_philosopher);
-	pthread_create(philo, NULL, 
+	printf("time test : usec: %d sec: %ld\n", time.tv_usec, time.tv_sec);
+	philo = malloc(sizeof(pthread_t) * 5);
+	i = -1;
+	while (++i < 5)
+		pthread_create(&philo[i], NULL, philo_loop, NULL);
+	monitoring_loop();
 
+	// exception (number of argc)
+	// init
+	// 	struct?
+	// 		number of philosophers
+	// 		time to die
+	// 		time to eat
+	// 		time to sleep
+	// 		number of must eat
+	// get_start_time
+	// while
+	// 	pthread create --> philo_loop
+	// monitoring_loop
+	// 	while(1)
+	// 		check_died
+	// 	finish_process
 }
 
-void	*
+void	*philo_loop(void *philo)
+{
+	// if (id == even)
+	// 	sleep
+	// while(1)
+	// 	get_fork
+	// 	eat
+	// 	sleep
+	// 	think
+}
--- inner_circle/philosophers/philo/philo.h
+++ inner_circle/philosophers/philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: siyang <siyang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 21:58:34 by siyang            #+#    #+#             */
-/*   Updated: 2023/04/08 22:04:40 by siyang           ###   ########.fr       */
+/*   Updated: 2023/04/14 17:17:04 by siyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,53 @@
 //  pthread_mutex_init, pthread_mutex_destroy
 //  pthread_mutex_lock, pthread_mutex_unlock
 # include <pthread.h>
+
+# define NUM 1
+# define DIE 2
+# define EAT 3
+# define SLEEP 4
+# define LIMIT 5
+
+typedef struct s_info
+{
+	// Read only(philos, monitor)
+	int				number_of_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				number_of_must_eat;
+	long long		start_time;
+
+	// Read(philos) & Write(monitor)
+	int				is_end;
+
+	// Read(philos) & Write(philos) => need mutex
+	int				*forks;
+
+	// philo vs philo vs monitor
+	pthread_mutex_t	m_print;
+
+	// philo vs philo
+	pthread_mutex_t	*m_forks;
+}	t_info;
+
+typedef struct s_philo
+{
+	// Read only(philo, monitor)
+	int				id;
+	pthread_t		philo;
+
+	// Read(monitor) & Write(philo)
+	int				number_of_eat;
+	long long		time_of_last_eat;
+
+	struct s_info	*info;
+}	t_philo;
+
+typedef struct s_monitor
+{
+	struct s_info	*info;
+	struct s_philo	*philos;
+}	t_monitor;
 
 #endif
